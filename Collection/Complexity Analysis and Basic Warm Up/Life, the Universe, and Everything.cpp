74cb86e1de3f46ccb8baf6b80405b9f33fbb656a
AJAY DANDGE <60701544+theInvincible@users.noreply.github.com>
2020-09-09 21:50:26 +0530

    Update Life, the Universe, and Everything.cpp

--- Collection/Complexity Analysis and Basic Warm Up/Life, the Universe, and Everything.cpp
+++ Collection/Complexity Analysis and Basic Warm Up/Life, the Universe, and Everything.cpp
@@ -21,13 +21,13 @@ Output:
 using namespace std;
 
 int main() {
-    int x;
-    while(1) {
-      cin>>x;
-      if(x!=42)
-        cout<<x<<endl;
-      else
-        break;
-    }
+  int x;
+  while(1) {
+    cin>>x;
+    if(x!=42)
+      cout<<x<<endl;
+    else
+      break;
+  }
 	return 0;
 }

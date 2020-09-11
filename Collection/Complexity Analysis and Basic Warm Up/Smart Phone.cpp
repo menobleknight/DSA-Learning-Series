af5bd405fbc1e3c30b771bdf183195dbe16f779f
AJAY DANDGE <60701544+theInvincible@users.noreply.github.com>
2020-09-11 20:47:53 +0530

    Create Smart Phone.cpp

---
+++ Collection/Complexity Analysis and Basic Warm Up/Smart Phone.cpp
@@ -0,0 +1,66 @@
+/*
+Zonal Computing Olympiad 2014, 30 Nov 2013
+
+You are developing a smartphone app. You have a list of potential customers for your app. 
+Each customer has a budget and will buy the app at your declared price if and only if the price is less than or equal to the customer's budget.
+You want to fix a price so that the revenue you earn from the app is maximized. Find this maximum possible revenue.
+
+For instance, suppose you have 4 potential customers and their budgets are 30, 20, 53 and 14. In this case, the maximum revenue you can get is 60 .
+
+Input format
+
+Line 1 : N, the total number of potential customers.
+
+Lines 2 to N+1: Each line has the budget of a potential customer.
+
+Output format
+
+The output consists of a single integer, the maximum possible revenue you can earn from selling your app.
+
+Sample Input 1
+4
+30
+20
+53
+14
+
+
+Sample Output 1
+60
+
+Sample Input 2
+5
+40
+3
+65
+33
+21
+
+Sample Output 2
+99
+
+Note:
+The answer might not fit in a variable of type int. We recommend that you use variables of type long long to read the input and compute the answer. 
+If you use printf and scanf, you can use %lld for long long. 
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int size;
+    long long result = 0, temp;
+    cin>>size;
+	long long *arr = new long long[size];
+	for (int i=0; i<size; i++) {
+	    cin>>arr[i];
+	}
+	sort(arr, arr+size);
+	for (int i=0; i<size; i++) {
+	    temp = arr[i] * (size-i);
+	    result = max(temp, result);
+	}
+	cout<<result<<endl;
+	return 0;
+}
